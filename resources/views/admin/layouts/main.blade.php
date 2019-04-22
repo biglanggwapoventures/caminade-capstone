41e9b7603533e1233b44e38b36231a3976524030
biglanggwapoventures <biglanggwapoventures@gmail.com>
2019-04-23 00:40:11 +0800

    change capstone title

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Pet Care</a>
+        <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
